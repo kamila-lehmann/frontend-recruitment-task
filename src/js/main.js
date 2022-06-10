1512fce9b5c830a393226aace6fc2d2cad2fc273
Kamila Lehmann <lehmann.europe@gmail.com>
2022-06-10 23:12:06 +0200

    Rearranged and adjusted styles, added sessionStorage to click count

--- src/js/main.js
+++ src/js/main.js
@@ -1,18 +1,36 @@
 const modal = document.getElementById("modal");
+const modalContent = document.getElementsByClassName("modal-content")[0];
 const btn = document.getElementById("btn");
 const span = document.getElementById("close");
+const displayCounter = document.getElementsByClassName("counter")[0];
+const resetBtn = document.getElementById("btn-reset");
 
-let counter = 0;
+// Store click count in sessionStorage, reset it when Reset button is clicked
+function countClicks() {
+  if (typeof(Storage) !== "undefined") {
+    if (sessionStorage.count) {
+      sessionStorage.count = Number(sessionStorage.count)+1;
+    } else {
+        sessionStorage.count = 1;
+    }
+    displayCounter.innerHTML = sessionStorage.count;
+    if (sessionStorage.count >= 5) {
+      resetBtn.style.opacity = 1;
+      resetBtn.addEventListener("click", () => {
+        sessionStorage.count = 0;
+        //count = 0;
+        modal.style.display = "none";
+      });
+    } else {
+      resetBtn.style.opacity = 0;
+    }
+  }
+}
 
-// Open the modal when user clicks a button
+// Open the modal when user clicks a button and count clicks
 btn.onclick = () => {
   modal.style.display = "block";
-  if (counter > 5) {
-    console.log(`You clicked ${counter} times`);
-  } else { 
-    counter++;
-    console.log(`You clicked ${counter} times`);
-  }
+  countClicks();
 }
 
 // Close the modal when user clicks "X"
@@ -22,7 +40,7 @@ span.onclick = () => {
 
 // Close the modal when user clicks anywhere
 window.onclick = (e) => {
-  if (event.target == modal) {
+  if (e.target == modal) {
     modal.style.display = "none";
   }
 }
